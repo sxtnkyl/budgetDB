63e88c3744fa5c360f51f14cbe0dab1535ac021a
kyle sexton <ksexton.exe@gmail.com>
2021-08-10 14:24:40 -0400

    async mongo connect

--- server.js
+++ server.js
@@ -11,12 +11,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+const linkDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost/budget",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+linkDB();
 
 // routes
 app.use(require("./routes/api.js"));
